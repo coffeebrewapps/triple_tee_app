3c035b6c37e1c46cf4e5e36f68e2b7ac79ec8d25
Coffee Brew Apps <coffeebrewapps@gmail.com>
2023-07-12 13:33:34 +0800

    Update config for demo url

--- client/src/config.js
+++ client/src/config.js
@@ -6,8 +6,10 @@ const useConfig = () => {
 
   if (env.MODE === 'development') {
     demoUrl.value = 'http://localhost:5173';
-  } else {
+  } else if (env.MODE === 'staging') {
     demoUrl.value = '/triple_tee_app-demo';
+  } else {
+    demoUrl.value = 'demo.tripletee.app';
   }
 
   return {
